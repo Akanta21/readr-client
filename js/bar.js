a8c67a0571ffe820628292aa58e2c90d426837a3
stephanie <hendricks.step@gmail.com>
2016-07-20 12:14:53 +0800

    adds extra large interactive search bar

---
+++ js/bar.js
@@ -0,0 +1,35 @@
+var search = document.getElementsByClassName('query-input')[0]
+
+search.addEventListener('keyup', function (e) {
+  var query = e.target.value
+  var results = document.getElementById('results')
+  results.classList = query === "" ? "" : "active"
+  searchLibrary(query)
+})
+search.addEventListener('focus', function(e) {
+  var main = document.getElementsByClassName('main')[0]
+  main.classList = 'main'
+})
+
+//cancel on X click or ESC
+var cancel = document.getElementsByClassName('cancel')[0]
+cancel.addEventListener('click', cancelInput)
+
+document.onkeydown = function (e) {
+  e = e || window.event
+  var isEscape = false
+  if ('key' in e) {
+    isEscape = e.key === 'Escape'
+  } else {
+    isEscape = e.keyCode === 27
+  }
+  if (isEscape) cancelInput()
+}
+
+function cancelInput () {
+  var main = document.getElementsByClassName('main')[0]
+  main.classList.add('inactive')
+  search.value = ''
+  document.getElementById('results').classList = ''
+  search.blur()
+}
