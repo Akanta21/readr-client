4c3cc54a7e60fda5a54bfc2f3666d2ce33d1d75b
David Tan <davified@gmail.com>
2016-07-21 00:13:53 +0800

    added topic query search. /topics-all.html?topic=nameOfTopic

--- js/topics.js
+++ js/topics.js
@@ -1,14 +1,9 @@
 /* global $ GRIDSLIDESHOW */
 
-// defining functions which are used in topics.js
-function getParameterByName (name, url) {
-  if (!url) url = window.location.href
-  name = name.replace(/[\[\]]/g, '\\$&')
-  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-    results = regex.exec(url)
-  if (!results) return null
-  if (!results[2]) return ''
-  return decodeURIComponent(results[2].replace(/\+/g, ' '))
+// reading URL params using jQuery
+$.urlParam = function (name) {
+  var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href)
+  return results[1] || 0
 }
 
 // defining a capitalize() function for article's title
@@ -27,8 +22,9 @@ function setCurrentGrid () {
 }
 
 function getArticlesByTopic () {
+  const topicName = $.urlParam('topic')
   $.ajax({
-    url: 'https://readr-app.herokuapp.com/articles/',
+    url: 'https://readr-app.herokuapp.com/topics/' + topicName,
     success: function (data) {
       var currentPage
       var currentColumn
@@ -36,7 +32,49 @@ function getArticlesByTopic () {
       var currentItem
       var currentTitle
       var counter = 0
-      console.log(data.articles[0]._id)
+      for (var i = 0; i < data.length; i++) {
+        if (i % 5 === 0) {
+          currentPage = $('<div class="grid grid--vertical grid--current grid--style-2" data-fill="#ece6e6">')
+          $('div.grid-pages').append(currentPage)
+        }
+
+        if (counter === 0) {
+          currentColumn = $('<div class="grid__column">')
+          currentPage.append(currentColumn)
+          counter = getRandomInt(1, 2)
+        }
+        counter--
+        current_Grid = $('<div class="grid__item" data-delay="50" style="transform: translateY(0px)">')
+        currentColumn.append(current_Grid)
+
+        currentItem = $('<div class="grid__img">')
+        current_Grid.append(currentItem)
+
+        currentItem.css('background-image', 'url(' + data[i].images[0] + ')')
+        currentItem.wrap('<a href="https://flight846.github.io/readr-client/articles/article.html?id=' + data[i]._id + '"></a>')
+
+        currentTitle = $('<h3 class="caption topic articleTitle" style="transform: translateY(0px); background: rgba(255,255,255,0.6); padding: 5px 20px 20px 5px">')
+        current_Grid.append(currentTitle)
+        currentTitle.append(data[i].title.toUpperCase())
+        currentTitle.wrap('<a href="https://flight846.github.io/readr-client/articles/article.html?id=' + data[i]._id + '"></a>')
+      }
+      setCurrentGrid()
+      GRIDSLIDESHOW()
+    }
+  })
+}
+
+// PUBLIC ROOT PAGE. GET ALL ARTICLES IN DATABASE, SORTED BY RECENCY
+function getAllArticles () {
+  $.ajax({
+    url: 'https://readr-app.herokuapp.com/articles',
+    success: function (data) {
+      var currentPage
+      var currentColumn
+      var current_Grid
+      var currentItem
+      var currentTitle
+      var counter = 0
       for (var i = 0; i < data.articles.length; i++) {
         if (i % 5 === 0) {
           currentPage = $('<div class="grid grid--vertical grid--current grid--style-2" data-fill="#ece6e6">')
@@ -60,7 +98,7 @@ function getArticlesByTopic () {
 
         currentTitle = $('<h3 class="caption topic articleTitle" style="transform: translateY(0px); background: rgba(255,255,255,0.6); padding: 5px 20px 20px 5px">')
         current_Grid.append(currentTitle)
-        currentTitle.append(data.articles[i].title.capitalize())
+        currentTitle.append(data.articles[i].title.toUpperCase())
         currentTitle.wrap('<a href="https://flight846.github.io/readr-client/articles/article.html?id=' + data.articles[i]._id + '"></a>')
       }
       setCurrentGrid()
@@ -71,5 +109,6 @@ function getArticlesByTopic () {
 
 // jQuery time!
 $(function () {
-  getArticlesByTopic()
+  getArticlesByTopic() // this works only if the URL contains a topic query (e.g. http://localhost:8080/topics/topics-all.html?topic=nofx)
+// getAllArticles ()
 })
