d402f233f4ba2f68ed6be426e8746ccd9d97d72e
David Tan <davified@gmail.com>
2016-07-20 08:27:13 +0800

    fixed font and background

--- js/topics.js
+++ js/topics.js
@@ -7,6 +7,7 @@ $(function () {
   getArticlesByTopic()
 })
 
+// for topics-white-theme.html
 function getArticlesByTopic () {
   $.ajax({
     url: 'https://readr-app.herokuapp.com/articles/',
@@ -14,11 +15,22 @@ function getArticlesByTopic () {
       console.log(data.article[0])
       for (var i = 0; i < 13; i++) {
         $('div.grid__item__' + i).css('background-image', 'url(' + data.article[i].images[0] + ')')
-        if (data.article[i].title.length > 60) {
-          $('div.grid__item__' + i).append(data.article[i].title.capitalize())
-        } else {
-          $('h3.topic__' + i).append(data.article[i].title.capitalize())
-        }
+        $('h3.topic__' + i).append(data.article[i].title.capitalize()).addClass('articleTitle')
+      // $('.grid__article').append(data.article[i].tldr[0].summary)
+      }
+    }
+  })
+}
+
+// for topics-all.html
+function addArticlesGrid () {
+  $.ajax({
+    url: 'https://readr-app.herokuapp.com/articles/',
+    success: function (data) {
+      console.log(data.article[0])
+      for (var i = 0; i < 13; i++) {
+        $('div.grid__item__' + i).css('background-image', 'url(' + data.article[i].images[0] + ')')
+        $('h3.topic__' + i).append(data.article[i].title.capitalize()).addClass('articleTitle')
       // $('.grid__article').append(data.article[i].tldr[0].summary)
       }
     }
