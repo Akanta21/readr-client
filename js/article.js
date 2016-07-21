9b88602f574531fbbbb67bf20c8afd47cae9a8df
Isabella Ong <isabellaong@gmail.com>
2016-07-21 12:58:25 +0800

    changes back to json

--- js/article.js
+++ js/article.js
@@ -34,7 +34,7 @@ $(document).ready(
     $.ajax({
       type: 'GET',
       url: serverURL + 'articles/' + id,
-      dataType: 'jsonp'
+      dataType: 'json'
     }).done(function (data) {
       console.log(data)
       $('#title').append(data.article.title.toUpperCase())
@@ -131,7 +131,7 @@ $(document).ready(
       $.ajax({
         type: 'POST',
         url: 'https://readr-app.herokuapp.com/articles',
-        dataType: 'jsonp',
+        dataType: 'json',
         data: data
       }).done(function (res) {
       // $('#title').empty().append(res.article.title.toUpperCase())
@@ -155,7 +155,7 @@ $(document).ready(
           $.ajax({
             type: 'PATCH',
             url: 'https://readr-app.herokuapp.com/articles/' + res.article._id,
-            dataType: 'jsonp',
+            dataType: 'json',
             data: data2
           }).done(function (res) {
           // console.log(res)
