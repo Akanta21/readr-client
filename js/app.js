ca0a787e5e597c00bc27429128ea96422d176c15
yazid <flight846@gmail.com>
2016-07-22 00:56:38 +0800

    Add redirect after sign up/in

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ function signup (formData) {
       window.localStorage.email = $('#inputEmail2').val()
       window.localStorage.auth_token = response.user.auth_token
       // then redirect
-      window.location.href = './index.html'
+      window.location.href = './articles/article.html'
     },
     error: function (xhr, ajaxOptions, thrownError) {
       // else output error
@@ -44,7 +44,7 @@ function signin (formData) {
       window.localStorage.email = $('#inputEmail2').val()
       window.localStorage.auth_token = response.user.auth_token
       // then redirect
-      window.location.href = './index.html'
+      window.location.href = './article/article.html'
     },
     error: function (xhr, ajaxOptions, thrownError) {
       // else output error
