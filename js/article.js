6970ad2ee304e1df809a6fe052075041d61c3f53
yazid <flight846@gmail.com>
2016-07-21 11:49:46 +0800

    Add topics to user

--- js/article.js
+++ js/article.js
@@ -2,7 +2,7 @@ console.log('article.js is loaded')
 /* global $ */
 $(document).ready(function () {
   var serverURL = 'https://readr-app.herokuapp.com/'
-
+  var currentUser = null || window.localStorage.id
   var id = '578e07585d16d1384ce6c2d1'
 
   $.ajax({
@@ -37,6 +37,16 @@ $(document).ready(function () {
           console.log(liked)
           // data.article.liked
         })
+        if (currentUser !== null) {
+          $.ajax({
+            type: 'PATCH',
+            url: serverURL + 'users/' + currentUser,
+            data: {articlesShared: id}
+          })
+          .done(function (data) {
+            console.log(data)
+          })
+        }
       })
   })
 
