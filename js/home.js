13e7e19baafbead8af2d3d2b7628188c09eff757
David Tan <davified@gmail.com>
2016-07-22 01:08:46 +0800

    /index.html done. yay

--- js/home.js
+++ js/home.js
@@ -1,4 +1,5 @@
 /* global $*/
+
 function getRandomInt (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
@@ -9,8 +10,7 @@ function createHomeGrid () {
     success: function (data) {
       console.log(data.articles)
       for (var i = 0; i < data.articles.length / 3; i = i + 3) {
-        var box = $("<main class='container'> <box class='flex-" + getRandomInt(1, 3) + "' id='box" + [i] + "'> </box> <box class='flex-" + getRandomInt(1, 3) + "' id='box" + [i + 1] + "'> </box> <box class='flex-" + getRandomInt(1, 3) + "' id='box" + [i + 2] + "'> </box> </main>")
-
+        var box = $("<grid class='container'> <box class='flex-" + getRandomInt(1, 3) + "' id='box" + [i] + "'> </box> <box class='flex-" + getRandomInt(1, 3) + "' id='box" + [i + 1] + "'> </box> <box class='flex-" + getRandomInt(1, 3) + "' id='box" + [i + 2] + "'> </box> </grid>")
         var container = $('div#homegrid')
         container.append(box)
       }
@@ -25,6 +25,9 @@ function getAllArticleTitles () {
       for (var i = 0; i < data.articles.length; i++) {
         var currentBox = $('box#box' + i)
         currentBox.append(data.articles[i].title)
+        currentBox.css('font-size', '2em')
+        currentBox.css('background-image', 'url(' + data.articles[i].images[0] + ')')
+        currentBox.wrapInner('<a href="https://flight846.github.io/readr-client/articles/article.html?id=' + data.articles[i]._id + '">')
       }
     }
   })
