e44dc585dd9309ba51dd48a4690740d70cbca8c8
yazid <flight846@gmail.com>
2016-07-22 01:07:23 +0800

    Add logout feature to article.html

--- js/app.js
+++ js/app.js
@@ -68,7 +68,7 @@ $(function () {
     // window.locationStorage.removeItem('id')
     window.localStorage.removeItem('email')
     window.localStorage.removeItem('auth_token')
-    window.location.href = './articles/article.html'
+    window.location.href = './article.html'
   })
 
   // listen for the form login
