3b1fbd8a77bfb26e4e3e00199d582785db7a32be
David Tan <davified@gmail.com>
2016-07-21 23:53:11 +0800

    aligned search bar for article.html

--- js/home.js
+++ js/home.js
@@ -8,8 +8,8 @@ function createHomeGrid () {
     url: 'https://readr-app.herokuapp.com/articles',
     success: function (data) {
       console.log(data.articles)
-      for (var i = 0; i < data.articles.length/3; i = i + 3) {
-        var box = $("<main class='container'> <box class='flex-" + getRandomInt(1,3) + "' id='box" + [i] + "'> </box> <box class='flex-" + getRandomInt(1,3) + "' id='box" + [i + 1] + "'> </box> <box class='flex-" + getRandomInt(1,3) + "' id='box" + [i + 2] + "'> </box> </main>")
+      for (var i = 0; i < data.articles.length / 3; i = i + 3) {
+        var box = $("<main class='container'> <box class='flex-" + getRandomInt(1, 3) + "' id='box" + [i] + "'> </box> <box class='flex-" + getRandomInt(1, 3) + "' id='box" + [i + 1] + "'> </box> <box class='flex-" + getRandomInt(1, 3) + "' id='box" + [i + 2] + "'> </box> </main>")
 
         var container = $('div#homegrid')
         container.append(box)
